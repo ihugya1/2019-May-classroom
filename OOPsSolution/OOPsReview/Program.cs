cba37cf4491355b1482dec68a67294409adb8a64
ihugya <ianhugya01@gmail.com>
2019-05-06 18:47:15 -0600

    create my first class

--- OOPsSolution/OOPsReview/Program.cs
+++ OOPsSolution/OOPsReview/Program.cs
@@ -9,8 +9,39 @@ namespace OOPsReview
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World");
-        }
-    }
-}
+        {   //console is a reference to a system static class
+            //static classes are used by typing the class name
+            //static classes do not store variable data
+            //static classes are used by everyone
+            //methods of a static class will be placed after
+            //  the object dot operator
+            Console.WriteLine("hello world");
+
+            //Create an instance of a non static class
+            //If you createa  declarative using JUST the 
+            //      class name then this is ONLY a pointer 
+            //      that can hold instance of the class
+            //      specified
+            //Currrent value of aVariable is null
+
+            myData aVariable=null;
+
+            //if you use the new command with the class
+            //      the new command will physically create
+            //      an instance of the class
+            myData anInstance = new myData();
+
+            if (aVariable==null)
+            {
+                Console.WriteLine("aVariable has nothing in it");
+            }
+            if (anInstance==null)
+            {
+                Console.WriteLine("anInstance has nothing in it");
+            }
+
+        }//eom(end of method)
+
+    }//eoc(end of class)
+
+}//eon(end of namespace)
---
+++ OOPsSolution/OOPsReview/myData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    //the default access permissions is private
+    //if an outside user of this class wanted 
+    //      access to the class contents; then 
+    //      the access permissions need to be public
+    //if this class was static; the class would not 
+    //      hold data
+    //For a class to hold data, DO NOT make it static
+    //      unless you want everyone to have access 
+    //      to that data(read and change)
+    public class myData
+    {
+        public int Height;
+        public int Width;
+
+    }
+}
