939ce2c65cf8178355fb486a866ca57bef609afe
ihugya1 <ihugya1@nait.ca>
2019-05-10 12:50:28 -0600

    Added the gate

---
+++ OOPsSolution/OOPsReview/Gate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+   public class Gate
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+        private string _gateStyle;
+
+        public string gateStyle
+        {
+            get
+            {
+                return _gateStyle;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _gateStyle = null;
+
+                }
+                else
+                {
+                    _gateStyle = value;
+                }
+               
+            }
+        }
+        //default
+        public Gate()
+        {
+
+        }
+        //greedy
+        public Gate(double height, double width, string style)
+        {
+            Height = height;
+            Width = width;
+            gateStyle = style;
+        }
+
+    }
+}
